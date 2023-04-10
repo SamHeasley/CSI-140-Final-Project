abc00f8b1c33ad55b7d0d86a5d52bffa0a5606ee
SamHeasley <68387016+SamHeasley@users.noreply.github.com>
2023-04-10 14:37:22 -0400

    Update main.cpp
    foundation implementation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+vector<int> renderBoard() {
+
+}
+
+int playerMove(int choice) {
+
+}
+
+int aiMove() {
+
+}
+
+bool checkWinner() {
+
+	return false;
+}
+
+int main() {
+	bool winner = false;
+
+	int playerChoice;
+
+	while (!winner) {
+		renderBoard();
+
+		cout << "Pick a column: ";
+		cin >> playerChoice;
+
+		playerMove(playerChoice);
+
+		winner = checkWinner();
+	}
+
+	return 0;
 }
