7bd667dad7c26b030f4af809ea19fb6df335ed0d
SamHeasley <68387016+SamHeasley@users.noreply.github.com>
2023-04-30 15:51:17 -0400

    Update Connect4.h

--- Connect4.h
+++ Connect4.h
@@ -5,13 +5,9 @@ namespace connectFour {
 	public:
 		bool winner = false;
 		bool playerTurn = true;
-		bool validOption;
 
 		char empty = 32;
 
 		std::vector<std::vector<char>> board = std::vector<std::vector<char>>(7, std::vector<char>(6, empty));
-
-		int playerChoice;
-		int gamemode;
 	};
 }
